579e18982e1dbdc1a65bbe31ba1ed478dbb534f3
jindalanshul01 <jindalanshul01@live.com>
2026-08-05 15:33:58 +0530

    initial spring boot project setup

---
+++ src/main/java/com/altius/clashcardtrader/ClashcardtraderApplication.java
@@ -0,0 +1,13 @@
+package com.altius.clashcardtrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashcardtraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashcardtraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altius/clashcardtrader/ClashcardtraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altius.clashcardtrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashcardtraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
